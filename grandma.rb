80d9b008846a385701b208d7abcab43e1f804453
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-18 05:16:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
+  ifphrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+  else phrase.downcase
+  "HUH?! SPEAK UP, SONNY!"
   end
 
 
