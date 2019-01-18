9e8242a029503aa620550568122a6e7662bd57cf
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-18 05:18:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
-    "NO, NOT SINCE 1938!"
-  else phrase.downcase
-  "HUH?! SPEAK UP, SONNY!"
-end
+
 
 
 # Whatever you say to grandma, she should respond with
